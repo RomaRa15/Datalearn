862239f1d8fe03d42fa54f878c353c7a68d4bb26
RomaRa15 <93906145+RomaRa15@users.noreply.github.com>
2021-11-14 20:53:42 +1000

    Поменял на название на английский

--- DE-101/Module2/Total_sales.sql
+++ DE-101/Module2/Total_sales.sql
@@ -1,18 +1,18 @@
-select round(sum(sales)) as Выручка_за_2016_год
+select round(sum(sales)) as Total_sales_2016
 from orders o 
 where date_part('year', order_date) = 2016;
 
-select round(sum(sales)) as Выручка_за_2017_год
+select round(sum(sales)) as Total_sales_2017
 from orders o 
 where date_part('year', order_date) = 2017;
 
-select round(sum(sales)) as Выручка_за_2018_год
+select round(sum(sales)) as Total_sales_2018
 from orders o 
 where date_part('year', order_date) = 2018;
 
-select round(sum(sales)) as Выручка_за_2019_год
+select round(sum(sales)) as Total_sales_2019
 from orders o 
 where date_part('year', order_date) = 2019;
 
-select round(sum(sales)) as Выручка_за_все_время
+select round(sum(sales)) as Total_sales
 from orders o;
