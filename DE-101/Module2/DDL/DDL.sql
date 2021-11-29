8ab988a51a82e1d91d7b895eba7916bedc7dd0c3
RomaRa15 <93906145+RomaRa15@users.noreply.github.com>
2021-11-29 23:57:11 +1000

    Update DDL.sql

--- DE-101/Module2/DDL/DDL.sql
+++ DE-101/Module2/DDL/DDL.sql
@@ -1,64 +1,93 @@
--- *************** SqlDBM: PostgreSQL ****************;
--- ***************************************************;
-
-
--- ************************************** calendar
-
-CREATE TABLE calendar
+/* создание таблицы calendar_dim */
+drop table if exist calendar_dim cascade
+CREATE TABLE calendar_dim
 (
  order_date date NOT NULL,
  ship_date  date NOT NULL,
- year       int4range NOT NULL,
+ year       int NOT NULL,
  quarter    varchar(5) NOT NULL,
- month      int4range NOT NULL,
- week       int4range NOT NULL,
- week_day   int4range NOT NULL,
+ month      int NOT NULL,
+ week       int NOT NULL,
+ week_day   int NOT NULL,
  CONSTRAINT PK_7 PRIMARY KEY ( order_date, ship_date )
 );
 
+/* добавление записей в таблицу calendar_dim */
+insert into calendar_dim 
+select order_date, ship_date, 
+date_part('year', order_date) as year, 
+date_part('quarter', order_date) as quarter, 
+date_part('month', order_date) as month, 
+date_part('week', order_date) as week, 
+EXTRACT(ISODOW FROM order_date) as week_day
+from (select distinct order_date, ship_date from stg.orders) a;
 
--- ************************************** geography
+/* создание таблицы product_dim */
+CREATE TABLE main.product_dim
+(
+ prod_id      serial NOT NULL,
+ product_id   varchar(25) NOT NULL,
+ category     varchar(25) NOT NULL,
+ subcategory  varchar(20) NOT NULL,
+ segment      varchar(20) NOT NULL,
+ product_name varchar(137) NOT NULL,
+ CONSTRAINT PK_29 PRIMARY KEY ( prod_id )
+);
 
-CREATE TABLE geography
+/* добавление уникальных записей товаров в таблицу product_dim */
+insert into main.product_dim
+select row_number() over (),
+product_id, category, subcategory, segment, product_name 
+from (select distinct on (product_id) product_id, category, subcategory, segment, product_name from stg.orders) a;
+
+/* создание таблицы geopraphy_dim */
+CREATE TABLE geography_dim
 (
  geo_id      serial NOT NULL,
  country     varchar(13) NOT NULL,
  city        varchar(17) NOT NULL,
- "state"       varchar(11) NOT NULL,
+ "state"       varchar(20) NOT NULL,
  region      varchar(7) NOT NULL,
- postal_code int4range NOT NULL,
+ postal_code int NULL,
  CONSTRAINT PK_49 PRIMARY KEY ( geo_id )
 );
 
+/* добавление уникальных записей географических данных в таблицу geography_dim_dim. в postal_code был NOT NULL и int4range -> выдавал ошибку, поменял на NULL и int*/
+insert into main.geography_dim
+select row_number () over (), country, city, state, region, postal_code
+from (select distinct country, city, state, region, postal_code from stg.orders) a;
 
--- ************************************** product
-
-CREATE TABLE product
-(
- product_id   serial NOT NULL,
- category     varchar(13) NOT NULL,
- subcategory  varchar(11) NOT NULL,
- segment      varchar(11) NOT NULL,
- product_name varchar(137) NOT NULL,
- CONSTRAINT PK_29 PRIMARY KEY ( product_id )
-);
-
-
--- ************************************** shipping
-
-CREATE TABLE shipping
+/* создание shipping_dim */
+CREATE TABLE shipping_dim
 (
  ship_id   serial NOT NULL,
  ship_mode varchar(14) NOT NULL,
  CONSTRAINT PK_69 PRIMARY KEY ( ship_id )
 );
 
+/* добавлению уникальных способов доставки, всего 4 */
+insert into main.shipping_dim
+select row_number () over (), ship_mode
+from (select distinct ship_mode from stg.orders o) a;
 
--- ***************************************************;
+
+/* создание таблицы клиентов */
+CREATE TABLE customer_dim
+(
+ cust_key      integer NOT NULL,
+ customer_id   varchar(30) NOT NULL,
+ customer_name varchar(50) NOT NULL,
+ CONSTRAINT PK_80 PRIMARY KEY ( cust_key )
+);
+
+/* добавлению уникальных клиентов */
+insert into customer_dim 
+select row_number () over(), customer_id, customer_name
+from (select distinct customer_id, customer_name
+from stg.orders o) a;
 
 
--- ************************************** sales_fact
-
+/* создание результирующей таблицы */
 CREATE TABLE sales_fact
 (
  order_id   varchar(14) NOT NULL,
@@ -68,15 +97,17 @@ CREATE TABLE sales_fact
  profit     numeric(21,16) NOT NULL,
  order_date date NOT NULL,
  ship_id    serial NOT NULL,
- product_id serial NOT NULL,
+ prod_id    serial NOT NULL,
  geo_id     serial NOT NULL,
  ship_date  date NOT NULL,
+ cust_key   integer NOT NULL,
  row_id     int4range NOT NULL,
  CONSTRAINT PK_16 PRIMARY KEY ( row_id ),
- CONSTRAINT FK_24 FOREIGN KEY ( order_date, ship_date ) REFERENCES calendar ( order_date, ship_date ),
- CONSTRAINT FK_38 FOREIGN KEY ( product_id ) REFERENCES product ( product_id ),
- CONSTRAINT FK_61 FOREIGN KEY ( geo_id ) REFERENCES geography ( geo_id ),
- CONSTRAINT FK_70 FOREIGN KEY ( ship_id ) REFERENCES shipping ( ship_id )
+ CONSTRAINT FK_24 FOREIGN KEY ( order_date, ship_date ) REFERENCES calendar_dim ( order_date, ship_date ),
+ CONSTRAINT FK_38 FOREIGN KEY ( prod_id ) REFERENCES product_dim ( prod_id ),
+ CONSTRAINT FK_61 FOREIGN KEY ( geo_id ) REFERENCES geography_dim ( geo_id ),
+ CONSTRAINT FK_70 FOREIGN KEY ( ship_id ) REFERENCES shipping_dim ( ship_id ),
+ CONSTRAINT FK_89 FOREIGN KEY ( cust_key ) REFERENCES customer_dim ( cust_key )
 );
 
 CREATE INDEX FK_26 ON sales_fact
@@ -87,7 +118,7 @@ CREATE INDEX FK_26 ON sales_fact
 
 CREATE INDEX FK_40 ON sales_fact
 (
- product_id
+ prod_id
 );
 
 CREATE INDEX FK_63 ON sales_fact
@@ -98,4 +129,9 @@ CREATE INDEX FK_63 ON sales_fact
 CREATE INDEX FK_72 ON sales_fact
 (
  ship_id
+);
+
+CREATE INDEX FK_91 ON sales_fact
+(
+ cust_key
 );
