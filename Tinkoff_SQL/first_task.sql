9424758268d41f0601dec80d52a84c0651df082f
RomaRa15 <93906145+RomaRa15@users.noreply.github.com>
2021-12-17 23:58:16 +1000

    Update first_task.sql

--- Tinkoff_SQL/first_task.sql
+++ Tinkoff_SQL/first_task.sql
@@ -1,4 +1,7 @@
-select f.second_pilot_id, name
+-- Задача 1. Напишите запрос, который выведет пилотов, которые в качестве второго пилота в
+-- августе этого года ездили в аэропорт Шереметьево. 
+
+select name
 from pilots p 
 	 join flights f on p.pilot_id = f.second_pilot_id
 where date_part('year', f.flight_dt) = 2021 and 
