c2887b8fb4cc01020227a229f5590eb04635f2b8
RomaRa15 <roman.rodionov.20@mail.ru>
2023-07-02 00:14:51 +1000

    made something

--- Analyst/Автомойка/Курсовая работа/controllers/workers.py
+++ Analyst/Автомойка/Курсовая работа/controllers/workers.py
@@ -3,16 +3,23 @@ from flask import render_template, request, session
 from datetime import datetime, timedelta
 #import sqlite3
 from utils import get_db_connection
-from models.workers_model import (get_workers, get_worker_name, get_dates)
+from models.workers_model import (get_workers, get_worker_name, get_dates, set_box_worker, get_worker_plan
+                                  ,get_all_plan)
 
 @app.route('/workers', methods=['get'])
 def workers():
     conn = get_db_connection()
     worker_id = ""
     box_id = ""
+    btn = 0
     first_date = datetime.today().strftime("%Y-%m-%d")
     second_date = (datetime.today() + timedelta(days=6)).strftime("%Y-%m-%d")
+    session['first_date'] = first_date
+    session['second_date'] = second_date
     
+    if request.values.get('btn'):
+        btn = int(request.values.get('btn'))
+        session['btn'] = btn
     if request.values.get('worker_id'):
         worker_id = int(request.values.get('worker_id'))
         session['worker_id'] = worker_id
@@ -25,20 +32,30 @@ def workers():
     if request.values.get('second_date'):
         second_date = request.values.get('second_date')
         session['second_date'] = second_date
+    if request.values.get('btn') == '3':
+        session['worker_id'] = ""
+        session['box_id'] = ""
 
     df_workers = get_workers(conn)
     df_worker_name = get_worker_name(conn, worker_id)
-    df_days = get_dates(conn, first_date, second_date)
+    df_days = get_dates(conn, session['first_date'], session['second_date'])
+    if request.values.get('worker_id') and request.values.get('box_id') and int(request.values.get('btn')) == 1:
+        df_set_box_worker = set_box_worker(conn, first_date, second_date, worker_id, box_id)
+    df_worker_plan = get_worker_plan(conn, worker_id, first_date, second_date)
+    df_all_plan = get_all_plan(conn, first_date, second_date)
 
     html = render_template(
     'workers.html',
     workers_list = df_workers,
     worker_id = session.get('worker_id'), 
     box_id = session.get('box_id'),
-    first_date = first_date,
-    second_date = second_date,
+    first_date = session.get('first_date'),
+    second_date = session.get('second_date'),
+    btn = session.get('btn'),
     worker_name = df_worker_name,
     days = df_days,
+    worker_plan = df_worker_plan,
+    all_plan = df_all_plan,
     len = len
     )
 
--- Analyst/Автомойка/Курсовая работа/models/workers_model.py
+++ Analyst/Автомойка/Курсовая работа/models/workers_model.py
@@ -27,3 +27,50 @@ def get_dates(conn, first_date, second_date):
     )
     SELECT dt FROM date_range;
     ''', conn, params={"b_first_date": first_date, "b_second_date": second_date})
+
+
+def set_box_worker(conn, first_date, second_date, worker_id, box_id):
+    cur = conn.cursor()
+
+    # Check if records already exist
+    cur.execute('''
+    SELECT COUNT(*) FROM box_worker WHERE worker_id = :b_worker_id AND box_id = :b_box_id AND work_date >= :b_first_date AND work_date <= :b_second_date;
+    ''', {"b_worker_id": worker_id, "b_box_id": box_id, "b_first_date": first_date, "b_second_date": second_date})
+    count = cur.fetchone()[0]
+
+    if count > 0:
+        # Records already exist, return without inserting
+        return
+
+    # Records do not exist, perform the insertion
+    cur.execute('''
+    INSERT INTO box_worker (worker_id, box_id, work_date)
+    WITH RECURSIVE date_range(work_date) AS (
+        SELECT date(:b_first_date)
+        UNION ALL
+        SELECT date(work_date, '+1 day')
+        FROM date_range
+        WHERE work_date < date(:b_second_date)
+    )
+    SELECT :b_worker_id worker_id, :b_box_id box_id, work_date
+    FROM date_range;
+    ''', {"b_first_date": first_date, "b_second_date": second_date, "b_worker_id": worker_id, "b_box_id": box_id})
+    conn.commit()
+
+
+def get_worker_plan(conn, worker_id, first_date, second_date):
+    # выбираем и выводим записи о бронированиях
+    return pandas.read_sql('''
+    SELECT *
+    FROM box_worker
+    WHERE worker_id = :b_worker_id AND ((work_date BETWEEN :b_first_date AND :b_second_date) OR
+                       work_date = :b_first_date)
+    ''', conn, params={"b_worker_id": worker_id, "b_first_date": first_date, "b_second_date": second_date})
+
+def get_all_plan(conn, first_date, second_date):
+    # выбираем и выводим записи о бронированиях
+    return pandas.read_sql('''
+    SELECT *
+    FROM box_worker JOIN worker USING (worker_id)
+    WHERE (work_date BETWEEN :b_first_date and :b_second_date) or work_date = :b_first_date
+    ''', conn, params={"b_first_date": first_date, "b_second_date": second_date})
