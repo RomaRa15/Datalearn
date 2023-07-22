92764572325fed9c28be4823aff9b3f2951e8d23
RomaRa15 <93906145+RomaRa15@users.noreply.github.com>
2023-07-22 20:02:16 +1000

    Удалил очистку ячеек + добавление к существ. строкам

--- birthList.js
+++ birthList.js
@@ -1,5 +1,5 @@
 function getUpcomingBirthdays() {
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var spreadsheet = SpreadsheetApp.openById("13N-sl98Xc1s4QDPXoppMZ5LIt9D9ZWHpZjK9XgA3RiE");
   var sheet = spreadsheet.getSheetByName("Sheet1"); // Замените "Sheet1" на имя вашего листа с данными о сотрудниках
   var birthListSheet = spreadsheet.getSheetByName("BirthList"); // Замените "BirthList" на имя вашего листа для списка дней рождений
 
@@ -19,7 +19,9 @@ function getUpcomingBirthdays() {
       var name = data[i][1]; // Индекс 1 соответствует столбцу B, где хранятся ФИО сотрудников
       var department = data[i][2]; // Индекс 2 соответствует столбцу C, где хранятся подразделения
       var email = data[i][16]; // Индекс 15 соответствует столбцу, где хранятся почты
-      birthdays.push([name, department, birthday, email]);
+      var moderation = '-'
+      var added_date = today
+      birthdays.push([name, department, birthday, email, moderation, added_date]);
     }
   }
 
@@ -36,14 +38,11 @@ function getUpcomingBirthdays() {
     return 0;
   });
 
-  // Очистка существующих значений в листе "BirthList"
-  birthListSheet.clearContents();
-
   if (birthdays.length > 0) {
-    var headers = ["Сотрудник.Физлицо", "Подразделение", "Дата рождения", "Почта"];
-    var outputData = [headers].concat(birthdays);
+    var lastRow = birthListSheet.getLastRow();
+    var startRow = lastRow + 1; // Add new data starting from the next row after the last one
 
-    var range = birthListSheet.getRange(1, 1, outputData.length, outputData[0].length);
-    range.setValues(outputData);
+    var range = birthListSheet.getRange(startRow, 1, birthdays.length, birthdays[0].length);
+    range.setValues(birthdays);
   }
 }
